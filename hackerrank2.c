490533bd26bf2927217c0a91820b5b77e556dc4e
Daksh-Sharma-02 <159519639+Daksh-Sharma-02@users.noreply.github.com>
2024-02-13 12:52:53 +0530

    Add files via upload

---
+++ hackerrank2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    float num1 = 10.0 ;
+    float num2 = 4.0;
+    int numm1 =(int)num1;
+    int numm2 =(int)num2;
+    printf("%d %d\n",numm1+numm2,numm1-numm2);
+
+    
+    float nnum1=4.0,nnum2=2.0;
+    printf("%.1f %.1f",nnum1+nnum2,nnum1-nnum2);
+    
+
+}
