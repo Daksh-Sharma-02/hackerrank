a6d4fa4e5fcdaab213436bb932040cbb4c003af3
Daksh-Sharma-02 <159519639+Daksh-Sharma-02@users.noreply.github.com>
2024-02-15 23:25:14 +0530

    Update hackerrankcode1.c

--- hackerrankcode1.c
+++ hackerrankcode1.c
@@ -1,52 +1,48 @@
-#include <stdio.h>
-
-int main() {
-    int start, end;
-
-    // Input the range
-    scanf("%d", &start);
-    scanf("%d", &end);
-
-    for (int i = start; i <= end; i++) {
-        if (i >= 1 && i <= 9) {
-            // Print the English representation for numbers 1 to 9
-            switch (i) {
-                case 1:
-                    printf("one\n");
-                    break;
-                case 2:
-                    printf("two\n");
-                    break;
-                case 3:
-                    printf("three\n");
-                    break;
-                case 4:
-                    printf("four\n");
-                    break;
-                case 5:
-                    printf("five\n");
-                    break;
-                case 6:
-                    printf("six\n");
-                    break;
-                case 7:
-                    printf("seven\n");
-                    break;
-                case 8:
-                    printf("eight\n");
-                    break;
-                case 9:
-                    printf("nine\n");
-                    break;
-            }
-        } else if (i % 2 == 0) {
-            // If even, print "even"
-            printf("even\n");
-        } else {
-            // If odd, print "odd"
-            printf("odd\n");
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    
+    switch(n){
+        case 1:{
+            printf("one");
+            break;
+        }
+        case 2:{
+            printf("two");
+            break;
+        }
+        case 3:{
+            printf("three");
+            break;
+        }
+        case 4:{
+            printf("four");
+            break;
+        }
+        case 5:{
+            printf("five");
+            break;
+        }
+        case 6:{
+            printf("six");
+            break;
+        }
+        case 7:{
+            printf("seven");
+            break;
+        }
+        case 8:{
+            printf("eight");
+            break;
+        }
+        case 9:{
+            printf("nine");
+            break;
+        }
+        default :{
+            printf("Greater than 9");
         }
     }
-
     return 0;
 }
