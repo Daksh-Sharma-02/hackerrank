f9327d0bb3cec49c1d62289b34048763a5f1c2a1
Daksh-Sharma-02 <159519639+Daksh-Sharma-02@users.noreply.github.com>
2024-02-15 23:26:40 +0530

    Update hackerrank2.c

--- hackerrank2.c
+++ hackerrank2.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
 int main(){
-    float num1 = 10.0 ;
-    float num2 = 4.0;
+    float num1;
+    scanf("%f",&num1);
+    float num2;
+    scanf("%f",&num2);
     int numm1 =(int)num1;
     int numm2 =(int)num2;
     printf("%d %d\n",numm1+numm2,numm1-numm2);
 
     
-    float nnum1=4.0,nnum2=2.0;
+    float nnum1;
+    scanf("%f",&nnum1);
+    float nnum2;
+    scanf("%f",&nnum2);
     printf("%.1f %.1f",nnum1+nnum2,nnum1-nnum2);
-    
+    return 0;
 
 }
